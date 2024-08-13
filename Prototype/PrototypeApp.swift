9a17ac77b4a84e06c3faca473c14b9ef9a3d0175
RachelRadford21 <rachelaradford21@gmail.com>
2024-08-13 09:02:41 -0400

    Initial Commit

---
+++ Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Rachel Radford on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
